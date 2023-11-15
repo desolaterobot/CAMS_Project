09319e2d44df62804758321c5d2027e64020a999
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-15 23:30:27 +0800

    Update DateStr.java
    Added javadoc to DateStr.java

--- DateStr.java
+++ DateStr.java
@@ -3,10 +3,17 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-//class that allows for conversion of Date to String
-//only for dd/mm/yyyy format!!!!
+/**
+ * Utility class for converting between Date and String representations in the "dd/MM/yyyy" format.
+ */
 public class DateStr {
 
+    /**
+     * Converts a string representation of a date to a Date object.
+     *
+     * @param str The string representation of the date in "dd/MM/yyyy" format.
+     * @return The corresponding Date object.
+     */
     public static Date strToDate(String str){
         DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         Date date = null;
@@ -19,6 +26,12 @@ public class DateStr {
         return date;
     }
 
+    /**
+     * Converts a Date object to a string representation.
+     *
+     * @param date The Date object to be converted.
+     * @return The string representation of the date in "dd/MM/yyyy" format.
+     */
     public static String dateToStr(Date date){
         DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         String str = null;
