4e365225b025a6baa318b9c6d6ee059198e81022
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-15 23:55:35 +0800

    Update Enquiry.java
    Added javadoc for Enquiry.java

--- Enquiry.java
+++ Enquiry.java
@@ -3,13 +3,30 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Represents an enquiry made by a student for a specific camp, along with the associated replies.
+ */
 public class Enquiry{
+    /** The unique identifier for the enquiry. */
     String enquiryID;
-    User student;
-    Camp camp;
-    String message;
-    private int[] replies; //will contain the reply ID's not the actual reply.
 
+    /** The user (student) making the enquiry. */
+    User student;
+
+    /** The camp associated with the enquiry. */
+    Camp camp;
+
+    /** The content of the enquiry message. */
+    String message;
+
+    /** An array of reply IDs associated with the enquiry. */
+    private int[] replies;
+
+    /**
+     * Gets an array of EnquiryReply objects corresponding to the replies associated with this enquiry.
+     *
+     * @return An array of EnquiryReply objects.
+     */
     public EnquiryReply[] getReplies(){
         List<EnquiryReply> replyList = new LinkedList<>();
         EnquiryReply[] database = EnquiryManager.getEnquiryReplyDatabase();
@@ -19,6 +36,12 @@ public class Enquiry{
         return replyList.toArray(new EnquiryReply[replyList.size()]);
     }
 
+    /**
+     * Adds a reply to the enquiry.
+     *
+     * @param replier The user providing the reply.
+     * @param reply   The content of the reply.
+     */
     public void reply(User replier, String reply){
         int replyID = EnquiryManager.getEnquiryReplyDatabase().length;
         String line = String.format("%d,%s,%s", replyID, CSVreader.removeCommas(reply), replier.userID);
@@ -30,7 +53,16 @@ public class Enquiry{
         String enqLine = String.format("%s,%s,%s,%s,%s", enquiryID, student.userID, CSVreader.removeCommas(camp.campName), CSVreader.removeCommas(message), CSVreader.listToString(newIDStrings));
         CSVreader.modifyLine("data/enquiry.csv", enquiryID, enqLine);
     }
-    
+
+    /**
+     * Constructs an Enquiry object with the specified parameters.
+     *
+     * @param enquiryID The unique identifier for the enquiry.
+     * @param student   The user (student) making the enquiry.
+     * @param camp      The camp associated with the enquiry.
+     * @param message   The content of the enquiry message.
+     * @param replies   An array of reply IDs associated with the enquiry.
+     */
     public Enquiry(String enquiryID, User student, Camp camp, String message, String[] replies) {
         this.enquiryID = enquiryID;
         this.student = student;
