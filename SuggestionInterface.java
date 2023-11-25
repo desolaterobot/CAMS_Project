b79c5e7067a66663098a489c5abbd79c5d2d2dd4
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-25 12:34:53 +0800

    Update SuggestionInterface.java
    updated javadoc for SuggestionInterface.java

--- SuggestionInterface.java
+++ SuggestionInterface.java
@@ -1,7 +1,24 @@
-
+/**
+ * The SuggestionInterface interface defines methods for managing suggestions.
+ */
 public interface SuggestionInterface {
+	 /**
+     * Submits a suggestion.
+     */
 	public void submitSuggestion();
+
+	/**
+     * Views suggestions submitted by the user.
+     */
 	public void viewOwnSuggestions();
+
+	/**
+     * Edits a submitted suggestion.
+     */
 	public void editSuggestion();
+
+	/**
+     * Deletes a submitted suggestion.
+     */
 	public void deleteSuggestion();
 }
