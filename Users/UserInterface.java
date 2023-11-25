2267b27a6d7f4a41231d5a19414829faf1460de8
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-25 22:38:48 +0800

    Update UserInterface.java
    updated userinterface.java

--- Users/UserInterface.java
+++ Users/UserInterface.java
@@ -1,6 +1,23 @@
 package Users;
 
+/**
+ * Represents the interface for user-related functionalities.
+ */
 public interface UserInterface {
+
+	/**
+     * Performs user login.
+     *
+     * @return The User object representing the logged-in user.
+     */
 	public User login();
+
+	/**
+     * Authenticates a user based on user ID and password.
+     *
+     * @param userId   The user ID to authenticate.
+     * @param password The password to authenticate.
+     * @return The User object if authentication is successful, null otherwise.
+     */
 	public User authUser(String userId, String password);
 }
