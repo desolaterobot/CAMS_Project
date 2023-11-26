97666757c50ebd7b58d44b6a62238d1a185b26d6
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-26 11:30:48 +0800

    Update EnquiryInterface.java
    updated javadoc for enquiryinterface.java

--- Enquiry/EnquiryInterface.java
+++ Enquiry/EnquiryInterface.java
@@ -1,9 +1,41 @@
 package Enquiry;
 
+/**
+ * An interface defining methods for managing enquiries.
+ */
 public interface EnquiryInterface {
+	
+	/**
+     * Displays a list of all enquiries.
+     */
 	public void viewEnquiries();
+
+	/**
+     * Displays details of a specific enquiry based on the provided enquiryID.
+     *
+     * @param enquiryID The unique identifier of the enquiry to view.
+     */
 	public void viewEnquiry(String enquiryID);
+
+	/**
+     * Submits a new enquiry with the provided name and message.
+     *
+     * @param name The name associated with the enquiry.
+     * @param msg  The message content of the enquiry.
+     */
 	public void submitEnquiry(String name, String msg);
+
+	/**
+     * Edits the content of a specific enquiry based on the provided enquiryID.
+     *
+     * @param enquiryID The unique identifier of the enquiry to edit.
+     */
 	public void editEnquiry(String enquiryID);
+
+	/**
+     * Deletes a specific enquiry based on the provided enquiryID.
+     *
+     * @param enquiryID The unique identifier of the enquiry to delete.
+     */
 	public void deleteEnquiry(String enquiryID);
 }
