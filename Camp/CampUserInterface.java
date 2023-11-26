c5075139da85abe6719c4ecf2378e48fd0c50513
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-26 11:22:16 +0800

    Update CampUserInterface.java
    updated javadoc for campuserinterface.java

--- Camp/CampUserInterface.java
+++ Camp/CampUserInterface.java
@@ -3,8 +3,27 @@ import Users.UserInterface;
 import Users.Student;
 import Users.Staff;
 
+/**
+ * Interface for handling user authentication within the camp management system.
+ */
 public interface CampUserInterface extends UserInterface {
+
+	/**
+     * Authenticate a student based on user ID and password.
+     *
+     * @param userId   The user ID of the student.
+     * @param password The password of the student.
+     * @return A Student object if authentication is successful; otherwise, null.
+     */
 	public Student authStudent(String userId, String password);
+
+	/**
+     * Authenticate a staff member based on user ID and password.
+     *
+     * @param userId   The user ID of the staff member.
+     * @param password The password of the staff member.
+     * @return A Staff object if authentication is successful; otherwise, null.
+     */
 	public Staff authStaff(String UserId, String password);
 
 }
