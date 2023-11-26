8b84edc8a69726037fe8a8208f59c8749e69842c
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-26 11:13:16 +0800

    Update Camp.java
    updated javadoc for camp.java

--- Camp/Camp.java
+++ Camp/Camp.java
@@ -7,13 +7,10 @@ import Users.UserManager;
  * Represents a camp with various attributes such as name, dates, location, and attendees.
  */
 public class Camp {
-    //all the variables listed here are NOT private as of now
-    //i want them to be accessible without the user of accessors for simplicity
     
     /** The name of the camp. */
     protected String campName;
     
-    //all of these are Date objects, not strings!
     /** The start date of the camp. */
     protected Date startDate; 
 
@@ -55,13 +52,13 @@ public class Camp {
     /** The number of slots reserved for committees (maximum 10). */
     protected int committeeSlots;
     
-    //faculty is automatically derived from the faculty of the staff creating this camp, see line 97.
     /**
      * The faculty of the staff member in charge of the camp.
      * It is automatically derived from the faculty of the staff creating this camp.
      */
     protected String faculty;
     
+    /** An array of names associated with the withdrawal of camp. */
     protected String[] withdrawals;
 
     /**
@@ -100,84 +97,177 @@ public class Camp {
         this.faculty = UserManager.getStaff(staffInCharge).getFaculty();
         this.withdrawals = withdrawals;
     }
-    
+
+    // Getters for the various attributes of the camp
+
+    /**
+     * Returns the name of the camp.
+     *
+     * @return The name of the camp.
+     */
     public String getCampName() {
         return campName;
     }
 
+    /**
+     * Returns an array of user IDs representing attendees of the camp.
+     *
+     * @return An array of user IDs representing attendees of the camp.
+     */
     public String[] getAttendees() {
         return attendees;
     }
 
+    /**
+     * Returns an array of committee names associated with the camp.
+     *
+     * @return An array of committee names associated with the camp.
+     */
     public String[] getCommitteeList() {
         return committeeList;
     }
     
-
+    /**
+ * Retrieves the faculty restriction status for the staff member in charge of the camp.
+ *
+ * @return true if the staff member is restricted to their own faculty, false otherwise.
+ */
     public boolean getOnlyFaculty() {
     	return this.onlyFaculty;
     }
 
+    /**
+     * Returns the number of slots reserved for committees.
+     *
+     * @return The number of slots reserved for committees.
+     */
     public int getCommitteeSlots() {
         return committeeSlots;
     }
-    
+
+    /**
+     * Returns the description of the camp.
+     *
+     * @return The description of the camp.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns the end date of the camp.
+     *
+     * @return The end date of the camp.
+     */
     public Date getEndDate() {
         return endDate;
     }
 
+    /**
+     * Returns the faculty of the staff member in charge of the camp.
+     *
+     * @return The faculty of the staff member in charge of the camp.
+     */
     public String getFaculty() {
         return faculty;
     }
 
+    /**
+     * Returns the location of the camp.
+     *
+     * @return The location of the camp.
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Returns the registration deadline for the camp.
+     *
+     * @return The registration deadline for the camp.
+     */
     public Date getRegistrationDeadline() {
         return registrationDeadline;
     }
 
+    /**
+     * Returns the staff member in charge of the camp.
+     *
+     * @return The staff member in charge of the camp.
+     */
     public String getStaffInCharge() {
         return staffInCharge;
     }
 
+    /**
+     * Returns the start date of the camp.
+     *
+     * @return The start date of the camp.
+     */
     public Date getStartDate() {
         return startDate;
     }
 
+    /**
+     * Returns the total number of slots available for the camp.
+     *
+     * @return The total number of slots available for the camp.
+     */
     public int getTotalSlots() {
         return totalSlots;
     }
 
+    /**
+     * Returns an array of user IDs representing withdrawals from the camp.
+     *
+     * @return An array of user IDs representing withdrawals from the camp.
+     */
     public String[] getWithdrawals() {
         return withdrawals;
     }
 
+    /**
+     * Returns a boolean indicating whether the camp is visible.
+     *
+     * @return A boolean indicating whether the camp is visible.
+     */
     public boolean getVisible() {
         return visible;
     }
 
+    // Methods for modifying camp attributes
+
+    /**
+     * Consumes a slot from the total available slots.
+     */
     public void consumeSlot() {
         totalSlots--;
     }
 
+    /**
+     * Adds a slot from the total available slots.
+     */
     public void releaseSlot() {
         totalSlots++;
     }
 
+     /**
+     * Toggles the visibility of the camp.
+     */
     public void toggleVisibility() {
         visible = !visible;
     }
 
+    /**
+     * Consumes a slot from the Committee member available slots.
+     */
     public void consumeCommitteeSlots() {
         committeeSlots--;
     }
-
+    
+    /**
+     * Adds a slot from the Committee member available slots.
+     */
     public void releaseCommitteeSlots() {
         committeeSlots++;
     }
