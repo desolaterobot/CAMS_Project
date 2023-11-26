ad02b43974257b5b62c40f44b93d175a2e632bbc
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-26 11:54:53 +0800

    Update UserInterface.java
    updated javadoc for userinterface.java

--- Users/UserInterface.java
+++ Users/UserInterface.java
@@ -1,6 +1,24 @@
 package Users;
 
+/**
+ * The UserInterface represents the interface for user-related operations in the system.
+ * Classes implementing this interface are responsible for handling user login and authentication.
+ */
 public interface UserInterface {
+	/**
+     * Prompts the user to log in and returns the corresponding user object upon successful login.
+     *
+     * @return The User object representing the logged-in user.
+     */
 	public User login();
+
+	/**
+     * Authenticates a user based on the provided user ID and password.
+     * Returns the authenticated User object if successful, or null if authentication fails.
+     *
+     * @param userId   The user ID for authentication.
+     * @param password The password for authentication.
+     * @return The authenticated User object, or null if authentication fails.
+     */
 	public User authUser(String userId, String password);
 }
