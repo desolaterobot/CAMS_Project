52c3e79c6aff210295297d18384a437e3152d14b
tsygan0011 <77485637+tsygan0011@users.noreply.github.com>
2023-11-26 11:36:47 +0800

    Update ApproveSuggestionInterface.java
    update javadoc for approvesuggestioninterface.java

--- Suggestion/ApproveSuggestionInterface.java
+++ Suggestion/ApproveSuggestionInterface.java
@@ -1,6 +1,16 @@
 package Suggestion;
 
+/**
+ * The ApproveSuggestionInterface provides methods for viewing camp suggestions and approving a suggestion.
+ */
 public interface ApproveSuggestionInterface{
+	/**
+     * Displays camp suggestions for viewing.
+     */
 	public void viewCampSuggestions();
+
+	/**
+     * Approves a camp suggestion.
+     */
 	public void approveSuggestion();
 }
